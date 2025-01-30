fec5e99af58c18b0876bccf5115a1c39c078773c
Bikash Chowdury <chowdhurybikash38@gmail.com>
2025-01-30 11:16:31 +0600

    initiated project django authentication system

---
+++ accounts/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager,AbstractBaseUser
+# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255,unique=True)
